0352918c7719583d00a28ae200877b75b652cdcb
Reaper47i <vineetbhengra96@gmail.com>
2022-02-19 01:02:23 +0530

    first commit 👍

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/layouts/Navbar"
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Navbar/>
+     Hello clone
+    </div>
+  )
+}
+
+export default App
