d69cffa94b52d2ab7117d2406a88caabc16f3497
Reaper47i <vineetbhengra96@gmail.com>
2022-02-20 15:04:58 +0530

    front-end on main branch 💕

--- src/components/layouts/Carousel.jsx
+++ src/components/layouts/Carousel.jsx
@@ -9,47 +9,82 @@ import enchanto from '../../../public/images/enchanto.webp'
 const images = [
     {
         id:'1',
+        name:'Shang-Chi and The Legend of Ten Rings',
+        genre:'Superhero',
+        year:'2020',
+        desc:'',
         img:'../../../public/images/ShangChi.webp'
     },
     {
         id:'2',
+        name:'Hawkeye',
+        genre:'Superhero',
+        year:'',
+        desc:'',
         img:'../../../public/images/hawkeye.webp'
     },
     {
         id:'3',
+        name:'Enchanto',
+        genre:'Fantasy',
+        year:'',
+        desc:'',
         img:'../../../public/images/enchanto.webp'
     },
     {
         id:'4',
+        name:'',
+        genre:'',
+        year:'',
+        desc:'',
         img:'../../../public/images/bookOfBOBFT.webp'
     },
     {
         id:'5',
+        name:'',
+        genre:'',
+        year:'',
+        desc:'',
         img:'../../../public/images/hawkeye.webp'
     },
     {
         id:'6',
+        name:'',
+        genre:'',
+        year:'',
+        desc:'',
         img:'../../../public/images/snowdrop.webp'
     },
 ];
 
 
 const settings = {
-    className:'center',
-    centerMode:true,
+    className: "center",
+    centerMode: true,
     infinite: true,
-    centerPadding: "60px",
-    speed: 500,
-    slidesToShow: 3,
+    centerPadding:'60px',
+    speed: 600,
+    slidesToShow: 1,
 };
 
 const Carousel = () => {
   return (
     <div className='cr'>
-        <Slider>
+        <Slider {...settings} >
             {images.map((item)=>{
                 return(
-                    <div className='items' key={item.id}><img src={item.img} alt={item.id} width='640px' height='360px' /></div>
+                    <div className='items' key={item.id}>
+                        <div className="items-container">
+                            <div className='desc'>
+                                <h1>{item.name}</h1>
+                                <h3>{item.genre}{item.year}</h3>
+                                <p>{item.desc}</p>
+                            </div>
+                            <div className="imgCon">
+                                <img src={item.img} alt={item.id} width='720px' height='411px'/>
+                            </div>
+                        </div>
+                    </div>
                 )
             })}
         </Slider>       
