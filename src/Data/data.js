3830262b0f71df1af4b076d918b732bf7daddd37
Reaper47i <vineetbhengra96@gmail.com>
2022-02-21 02:02:25 +0530

    modifications

---
+++ src/Data/data.js
@@ -0,0 +1,51 @@
+
+export const images = [
+    {
+        id:'1',
+        name:'Shang-Chi and The Legend of Ten Rings',
+        genre:'Superhero',
+        year:'2020',
+        desc:'',
+        img:'../../../images/ShangChi.webp'
+    },
+    {
+        id:'2',
+        name:'Hawkeye',
+        genre:'Superhero Marvel Action',
+        year:'2021',
+        desc:'',
+        img:'../../../images/hawkeye.webp'
+    },
+    {
+        id:'3',
+        name:'Enchanto',
+        genre:'Fantasy Family',
+        year:'2021',
+        desc:'',
+        img:'../../../images/enchanto.webp'
+    },
+    {
+        id:'4',
+        name:'The Book Of Boba Fet',
+        genre:'Sci-Fi',
+        year:'2021',
+        desc:'',
+        img:'../../../images/bookOfBOBFT.webp'
+    },
+    {
+        id:'5',
+        name:'Ice Age Adventures Of Buck Wild',
+        genre:'Family',
+        year:'2022',
+        desc:'',
+        img:'../../../images/iceAgeABW.webp'
+    },
+    {
+        id:'6',
+        name:'Snowdrop',
+        genre:'K-Drama Romance',
+        year:'2022',
+        desc:'',
+        img:'../../../images/snowdrop.webp'
+    },
+];
--- src/components/layouts/LpMainContent.jsx
+++ src/components/layouts/LpMainContent.jsx
@@ -5,27 +5,27 @@ const LpMainContent = () => {
     <div className="container flex-col center LpMain">
       <div className="lists container flex-col center">
         <h3>Recomended For You</h3>
-        <div className="list-items">items</div>
+        <div className="list-items flex center">items</div>
       </div>
       <div className="lists container flex-col center">
         <h3>New To Disney+</h3>
-        <div className="list-items">items</div>
+        <div className="list-items flex center">items</div>
       </div>
       <div className="lists container flex-col center">
         <h3>Hit Movies</h3>
-        <div className="list-items">items</div>
+        <div className="list-items flex center">items</div>
       </div>
       <div className="lists container flex-col center">
         <h3>Documentaries</h3>
-        <div className="list-items">items</div>
+        <div className="list-items flex center">items</div>
       </div>
       <div className="lists container flex-col center">
         <h3>Animals and Nature</h3>
-        <div className="list-items">items</div>
+        <div className="list-items flex center">items</div>
       </div>
       <div className="lists container flex-col center">
         <h3>Action Adventure</h3>
-        <div className="list-items">items</div>
+        <div className="list-items flex center">items</div>
       </div>
     </div>
   );
