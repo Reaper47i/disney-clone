924b1c934580421fcb31c5bb9c6c8421003e56d1
Reaper47i <vineetbhengra96@gmail.com>
2022-02-23 20:29:02 +0530

    some changes

--- src/components/images.js
+++ src/components/images.js
@@ -2,6 +2,6 @@ import disney from '../../public/images/disneyV.png';
 import natGeo from '../../public/images/natGeo.png';
 import marvel from '../../public/images/Marvel.png';
 import starWars from '../../public/images/starWars.png'
+import Pixar from '../../public/images/pixar 1.png';
 
-
-export {disney, natGeo,marvel,starWars};
+export {disney, natGeo,marvel,starWars,Pixar};
--- src/components/layouts/SelectorBar.jsx
+++ src/components/layouts/SelectorBar.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import {disney,natGeo,marvel,starWars} from '../images';
+import {disney,natGeo,marvel,starWars,Pixar} from '../images';
 const SelectorBar = () => {
   return (
     <div className="selector">
       <div className="select-bx flex center">
         <img src={disney} alt="" />
       </div>
-      <div className="select-bx flex center">Pixar</div>
+      <div className="select-bx flex center">
+        <img src={Pixar} alt="" />
+      </div>
       <div className="select-bx flex center">
         <img src={marvel} alt=""/>
       </div>
       <div className="select-bx flex center">
-        {/* <img src={starWars} alt="" width='100%' height='100%'/> */}
+        <img src={starWars} alt=""/>
       </div>
       <div className="select-bx flex center">
         <img src={natGeo} alt=""/>
